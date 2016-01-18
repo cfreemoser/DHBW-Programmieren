b3fbd9b61eb395981bcda504980896a6196fc9ee
Cem Philipp Freimoser <cem.freimoser@1und1.de>
2016-01-18 11:46:10 +0100

    bugfix

--- src/de/dhbwka/java/exercise/operatorn/Easter.java
+++ src/de/dhbwka/java/exercise/operatorn/Easter.java
@@ -1,13 +1,9 @@
 package de.dhbwka.java.exercise.operatorn;
 
 import de.dhbwka.java.exercise.utility.Tuple;
-import javafx.util.Pair;
-
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -15,13 +11,13 @@ import java.util.Scanner;
  */
 public class Easter {
 
-    private final  static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy");
+    private final  static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM y");
 
     public static void main(String[] args){
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Enter a Year");
-        System.out.println("easter is on " + makeDate(calculateEaster(myScanner.nextInt())));
-
+        System.out.println("Easter is on " + makeDate(calculateEaster(myScanner.nextInt())) + "\n");
+        main(null);
     }
 
     private static Tuple<Double, Integer> calculateEaster(int year) {
@@ -31,15 +27,16 @@ public class Easter {
         int k = year / 100;
         int p = (8 * k + 13) / 25;
         int q = k / 4;
-        int m = (15 + k - p - q) / 30;
-        int n = (4 + k - q) / 7;
+        int m = (15 + k - p - q) % 30;
+        int n = (4 + k - q) % 7;
         int d = (19*a + m) % 30;
-        double e = (2* b + 4 *c + 6d + n) % 7;
-        return new Tuple<>(22 + d + e, year);
+        double e = (2* b + 4 *c + 6*d + n) % 7;
+        double easter = 22 + d + e;
+        return new Tuple<>(easter, year);
     }
 
     private static String makeDate(Tuple<Double, Integer> tuple) {
-       LocalDate date = LocalDate.of(tuple.y, Month.MARCH, 1).plusDays(Math.round(tuple.x));
+       LocalDate date = LocalDate.of(tuple.y, Month.MARCH, 1).plusDays(Math.round(tuple.x -1));
         return date.format(formatter);
     }
 }
