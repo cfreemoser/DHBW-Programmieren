807222721b4a2ed50fb76bf1040125db2697fb1c
cemig <christoph-emig@web .de>
2016-02-08 09:27:36 +0100

    Ersteller berichtigt ;)

--- src/de/dhbwka/java/exercise/arrays/BubbleSort.java
+++ src/de/dhbwka/java/exercise/arrays/BubbleSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * Created by cem on 05.02.16.
+ * Created by cem on 05.02.16. Eigentlich hab ich das erstellt (cemig) 
  */
 public class BubbleSort {
 
